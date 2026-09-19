935f45726d856b1202c727e4d9d4b9a74ebb86f0
jeanniedupont0025-hub <jeanniedupont0025@gmail.com>
2026-09-19 10:32:14 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const crypto = require("crypto");
 const app = express();
 
 app.use(cors({
-  origin: "https://ecoll-ins.onrender.com"
+  origin: "https://econloa-ns.onrender.com"
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
